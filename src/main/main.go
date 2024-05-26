82f3ba0d14344b84e4200a6f5bd6ded48c604a36
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2024-05-26 08:11:28 -0400

    refactor(main): clean up function that execs git

--- src/main/main.go
+++ src/main/main.go
@@ -15,58 +15,60 @@ func main() {
 	args := cli.ParseCmdArgs()
 
 	var err error
-	var gitPath string
-
-	gitPath, err = exec.LookPath("git")
-	handle_error(err, "Executable `git` not found!")
+	runGitCommand := gitCommandRunner()
 
 	err = os.MkdirAll(args.LocalDir, os.FileMode(0o644))
-	handle_error(err, "Could not make directory: %s", args.LocalDir)
+	handleError(err, "Could not make directory: %s", args.LocalDir)
 
 	err = os.Chdir(args.LocalDir)
-	handle_error(err, "Could not cd to directory: %s", args.LocalDir)
+	handleError(err, "Could not cd to directory: %s", args.LocalDir)
 
-	run_git_command(gitPath, "init")
-	run_git_command(gitPath, "remote", "add", "-f", "origin", args.Url)
-	run_git_command(gitPath, "config", "core.sparseCheckout", "true")
+	runGitCommand("init")
+	runGitCommand("remote", "add", "-f", "origin", args.Url)
+	runGitCommand("config", "core.sparseCheckout", "true")
 
-	write_sparse_paths(args.Paths)
+	writeSparsePaths(args.Paths)
 
-	run_git_command(gitPath, "pull", "origin", args.Branch)
+	runGitCommand("pull", "origin", args.Branch)
 
 }
-func write_sparse_paths(paths []string) {
+func writeSparsePaths(paths []string) {
 	f, err := os.OpenFile(path.Join(".git", "info", "sparse-checkout"), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	defer f.Close()
 
-	handle_error(err, "")
+	handleError(err, "")
 	for i := 0; i < len(paths); i++ {
 		f.WriteString(paths[i] + "\n")
 	}
 }
-func run_git_command(gitPath string, args ...string) {
-	cmd := exec.Command(gitPath, args...)
+func gitCommandRunner() func(args ...string) {
+	gitPath, err := exec.LookPath("git")
+	handleError(err, "Executable `git` not found!")
 
-	var wg sync.WaitGroup
-	if stdout, err := cmd.StdoutPipe(); err == nil {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			io.Copy(os.Stdout, stdout)
-		}()
-	}
-	if stderr, err := cmd.StderrPipe(); err == nil {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			io.Copy(os.Stderr, stderr)
-		}()
-	}
+	return func(args ...string) {
+		cmd := exec.Command(gitPath, args...)
 
-	cmd.Run()
-	wg.Wait()
+		var wg sync.WaitGroup
+		if stdout, err := cmd.StdoutPipe(); err == nil {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				io.Copy(os.Stdout, stdout)
+			}()
+		}
+		if stderr, err := cmd.StderrPipe(); err == nil {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				io.Copy(os.Stderr, stderr)
+			}()
+		}
+
+		cmd.Run()
+		wg.Wait()
+	}
 }
-func handle_error(err error, pattern string, args ...any) {
+func handleError(err error, pattern string, args ...any) {
 	if err != nil {
 		if len(pattern) > 0 {
 			fmt.Fprintf(os.Stderr, pattern+"\n", args...)
